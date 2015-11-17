9058c407ed84f2bac85e153f4b4d4bba759a767e
Andreas Loeffler <aloeff@de.ibm.com>
2015-11-18 00:09:09 +0100

    code format changes only

--- app.js
+++ app.js
@@ -24,18 +24,18 @@ function normalizePort(val) {
 var port = normalizePort(process.env.PORT || '30331');
 server.listen(port);
 
-mongoose.connect('mongodb://localhost/chat', function(err){
-	if(err){
-		console.log(err);
-	} else{
-		console.log('Connected to mongodb!');
-	}
+mongoose.connect('mongodb://localhost/chat', function(err) {
+    if(err){
+	console.log(err);
+    } else{
+	console.log('Connected to mongodb!');
+    }
 });
 
 var chatSchema = mongoose.Schema({
-	nick: String,
-	msg: String,
-	created: {type: Date, default: Date.now}
+    nick: String,
+    msg: String,
+    created: {type: Date, default: Date.now}
 });
 
 var Chat = mongoose.model('Message', chatSchema);
@@ -44,8 +44,8 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
-app.get('/', function(req, res){
-        res.sendfile(__dirname + '/views/index.html');
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/views/index.html');
 });
 
 
@@ -60,9 +60,9 @@ io.on('connection', function(socket) {
 	socket.emit('load old msgs', docs);
     });
 
-    socket.on('new user', function(data, callback){
+    socket.on('new user', function(data, callback) {
 	var nick = data.trim();
-	if (!nick || (nick in users)){
+	if (!nick || (nick in users)) {
 	    callback(false);
 	} else {
 	    callback(true);
@@ -72,11 +72,11 @@ io.on('connection', function(socket) {
 	}
     });
 
-    function updateNicknames(){
+    function updateNicknames() {
 	io.sockets.emit('usernames', Object.keys(users));
     }
 
-    socket.on('send message', function(data, callback){
+    socket.on('send message', function(data, callback) {
 	var msg = data.trim();
 	console.log('after trimming message is: ' + msg);
 	var newMsg = new Chat({msg: msg, nick: socket.nickname});
@@ -87,7 +87,7 @@ io.on('connection', function(socket) {
 	});
     });
 
-    socket.on('disconnect', function(data){
+    socket.on('disconnect', function(data) {
 	if(!socket.nickname) return;
 	delete users[socket.nickname];
 	updateNicknames();
